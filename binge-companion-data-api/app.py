e9126891c3febe2653c051d398143c3a30a27614
gpelayo <gpelayo109@gmail.com>
2022-06-19 18:58:03 -0700

    Wrote basic api endpoints for tables

---
+++ binge-companion-data-api/app.py
@@ -0,0 +1,32 @@
+from chalice import Chalice
+from database import DatabaseConnection
+from chalice import BadRequestError
+
+app = Chalice(app_name='binge-companion-api')
+
+
+@app.route('/v1/series')
+def series():
+    with DatabaseConnection() as db:
+        return db.list_series()
+
+
+@app.route('/v1/episode')
+def episodes():
+    series_id = app.current_request.query_params.get('series-id')
+    season = app.current_request.query_params.get('season')
+
+    if season and series_id:
+        with DatabaseConnection() as db:
+            return db.get_episodes_from_season(series_id, season=season)
+    else:
+        raise BadRequestError
+
+
+@app.route('/v1/trivia')
+def trivia():
+    episode_id = app.current_request.query_params.get('episode-id')
+
+    if episode_id:
+        with DatabaseConnection() as db:
+            return db.get_trivia_from_episode(episode_id)
